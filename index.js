5e010bfa0028fba992dbba2fce21b68bb67efed2
Edison Fabian Molina Herrera - Ceiba Software <edison.molina@ceiba.com.co>
2022-06-18 10:05:51 -0500

    pasamos a produccion

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const {getConnection} = require('./db/db-conection-mongo');
 const app = express();
 const port = process.env.PORT || 5000;
 
-
-
 getConnection();
 
 //parseo a Json
@@ -16,7 +14,6 @@ app.use('/estado-equipo', require('./router/estadoEquipo'));
 app.use('/marca', require('./router/marca'));
 app.use('/tipo-equipo', require('./router/tipoEquipo'));
 app.use('/inventario', require('./router/inventario'));
-
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
