79c5ae12f4777456c803bd4075441fd3cc5c208e
Edison Fabian Molina Herrera - Ceiba Software <edison.molina@ceiba.com.co>
2022-06-18 11:07:16 -0500

    permiso CORS

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const {getConnection} = require('./db/db-conection-mongo');
-
+const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors())
 
 getConnection();
 
